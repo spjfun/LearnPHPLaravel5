487c2428ce8744c5a31362076095065b693d3e68
peijyunshih <peijyun.shih@gmail.com>
2016-08-01 10:47:23 +0800

    Schema類別來建立資料表,使用migrate指令來建立資料表 php artisan migrate

--- LearnLaravel5/database/migrations/2016_08_01_023220_create_posts_table.php
+++ LearnLaravel5/database/migrations/2016_08_01_023220_create_posts_table.php
@@ -5,6 +5,9 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreatePostsTable extends Migration
 {
+
+
+    //在終端機輸入 php artisan migrate, 會執行up()方法來建立資料表。
     /**
      * Run the migrations.
      *
@@ -12,9 +15,20 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        //
+        //Schema 類別可以操控資料表，使用 create() 方法建立，使用 drop() 方法移除。
+        //create 的第一個參數是資料表的名稱 posts。第二個參數則是要建立的欄位內容。
+        Schema::create('posts', function($table){
+            //increments 是唯一值的遞增數字，就是主鍵
+            $table->increments('id');
+            $table->string('title');
+            $table->string('content');
+            //timestamps 是時間戳記，在 Laravel 中預設是開啟的，可以在 Model 中關閉。
+            $table->timestamps();
+        });
     }
 
+
+    //在終端機輸入 php artisan migrate:rollback, 會執行down()來移除這個資料表
     /**
      * Reverse the migrations.
      *
@@ -22,6 +36,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('posts');
     }
 }
