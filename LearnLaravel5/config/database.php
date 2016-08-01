1a83895da871f882568841cc13886e35f823cf77
peijyunshih <peijyun.shih@gmail.com>
2016-08-01 10:30:38 +0800

    使用Migration設定資料庫配置檔, .env以及database.php

--- LearnLaravel5/config/database.php
+++ LearnLaravel5/config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'blog'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', '1234'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
